d7c89eb336b84e9600aecf0668b0692e6c0444c2
Tomasz Wojewódka <tomasz.wojewodka@gmail.com>
2020-05-24 07:56:40 +0200

    Add proper packaging and dependencies; Use frontend plugin to build Angular app

---
+++ service/src/main/java/com/hudela/vanilla/service/ForwardController.java
@@ -0,0 +1,12 @@
+package com.hudela.vanilla.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForwardController {
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/";
+    }
+}
